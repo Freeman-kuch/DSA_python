da959b9331993c048a38a32170d53f173dc72c84
Freeman <freemanoparaocha@gmail.com>
2023-11-10 11:21:48 +0100

    added some smart implementation of the solution by others

--- Roman_to_int.py
+++ Roman_to_int.py
@@ -37,3 +37,22 @@ class Solution:
             cache = romans[char]
 
         return sum
+
+
+"""class Solution:
+    def romanToInt(self, s: str) -> int:
+        decimal = 0
+        prev_value = 0
+        dict1 ={"I": 1,"V":5,"X":10,"L":50,"C":100, "D":500, "M" :1000}
+        lenght= len(s)
+        for x in reversed(s) :
+            value = dict1.get(x, 0)
+
+            if value < prev_value:
+                
+                decimal -= value
+            else:
+                decimal += value
+
+            prev_value = value
+        return decimal"""
